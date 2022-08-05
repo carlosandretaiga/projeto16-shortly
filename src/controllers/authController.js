91aa37189de21756ce149a26552887c898c9b6c2
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-08-05 15:58:55 -0300

    feat: add create tables from sortlydb

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,11 +2,27 @@ import db from '../database/db.js';
 import jwt from 'jsonwebtoken';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
+import dotenv from "dotenv";
+dotenv.config();
+
+const { JWT_SECRET_KEY } = process.env;
 
 
 export async function createUser(req, res) {
-  const user = req.body;
-  console.log(user);
+
+  try {
+
+    const {name, email, password, confirmPaswword} = req.body;
+    console.log(user);
+
+    const passwordEncrypted = bcrypt.hashSync(user.password, 10);
+    const confirmPasswordEncrypted = bcrypt.hashSync(user.confirmPaswword, 10);
+    
+
+    res.status(201).send("user created sucessfully!");
+  } catch (error) {
+    
+  }
 
   
 }
---
+++ src/database/createTables.sql
@@ -0,0 +1,30 @@
+--Criação do banco de dados 
+CREATE DATABASE "shortlydb";
+
+--TABLE users
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+  'email' VARCHAR (255) NOT NULL UNIQUE,
+  "password" TEXT NOT NULL,
+  "confirmPassword" TEXT NOT NULL,
+  "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+--TABLE urls
+CREATE TABLE "urls" (
+	"id" SERIAL PRIMARY KEY,
+	"url" TEXT NOT NULL,
+  "shortUrl" TEXT NOT NULL UNIQUE,
+  "visitCount" INTEGER NOT NULL DEFAULT 0,
+  "userId" INTEGER REFERENCES users(id),
+  "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+--TABLE urlsCount
+--CREATE TABLE "urlsCount" (
+	--"id" SERIAL PRIMARY KEY,
+  --"linksCount" INTEGER NOT NULL DEFAULT 0,
+  --"urlId" INTEGER REFERENCES urls(userId),
+  --"createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+--);
